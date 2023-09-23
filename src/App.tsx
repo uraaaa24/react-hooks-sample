b5126603266030c64c96e57cb170c1a37f0801c5
Yuto Urashima <yuto.urashima24@gmail.com>
2023-09-24 02:02:13 +0900

    useStateの使い方整理

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { Button, Divider, List, ListItem, Typography } from "@mui/material"
+
+import { useState } from "react"
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState<number>(0)
+
+  const handleClick = () => {
+    setCount(count + 1)
+  }
 
   return (
     <>
-      <h1>useState</h1>
+      <List>
+        <ListItem sx={{display: "flex", flexDirection: "column", gap: 2}}>
+          <Typography variant="h2">useState</Typography>
+          <Button variant="outlined" onClick={handleClick}>+</Button>
+          <Typography variant="body1">{count}</Typography>
+        </ListItem>
+        <Divider />
+
+      </List>
     </>
   )
 }
