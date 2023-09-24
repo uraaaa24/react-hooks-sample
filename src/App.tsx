ef9ade9df47569f762b7c04c376077d654d4a6ce
Yuto Urashima <yuto.urashima24@gmail.com>
2023-09-24 10:51:42 +0900

    useContextの使い方整理

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Button, Divider, List, ListItem, Typography } from "@mui/material"
 
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import './App.css'
+import { UserInfoContext } from "./main"
 
 function App() {
   const [count, setCount] = useState<number>(0)
+  const userInfo = useContext(UserInfoContext)
 
   const handleClick = () => {
     setCount(count + 1)
@@ -18,13 +20,20 @@ function App() {
     <>
       <List>
         <ListItem sx={{display: "flex", flexDirection: "column", gap: 2}}>
-          <Typography variant="h2">useState, useEffect</Typography>
+          <Typography variant="h3">useState, useEffect</Typography>
           <Button variant="outlined" onClick={handleClick}>+</Button>
           <Typography variant="body1">{count}</Typography>
         </ListItem>
         <Divider />
 
-      </List>
+        <ListItem sx={{display: "flex", flexDirection: "column", gap: 2}}>
+          <Typography variant="h3">useContext</Typography>
+          <Typography variant="body1">{userInfo.name}</Typography>
+          <Typography variant="body1">{userInfo.age}</Typography>
+        </ListItem>
+        <Divider />
+
+      </List>      
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+type UserInfoType = {
+  name: string,
+  age: number
+}
+
+const userInfo = {
+  name: "山田太郎",
+  age: 25
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const UserInfoContext = createContext<UserInfoType>(userInfo)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <UserInfoContext.Provider value={userInfo}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </UserInfoContext.Provider>
 )
