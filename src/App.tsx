ef7ac0c387cd931fdfdec6c9fb18ade9acf2fd9b
Yuto Urashima <yuto.urashima24@gmail.com>
2023-09-24 11:19:55 +0900

    useRef作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,55 @@
-import { Button, Divider, List, ListItem, Typography } from "@mui/material"
+import { Button, Divider, List, ListItem, TextField, Typography } from '@mui/material'
 
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect, useRef, useState } from 'react'
 import './App.css'
-import { UserInfoContext } from "./main"
+import { UserInfoContext } from './main'
 
 function App() {
   const [count, setCount] = useState<number>(0)
   const userInfo = useContext(UserInfoContext)
+  const ref = useRef<HTMLInputElement>()
 
   const handleClick = () => {
     setCount(count + 1)
   }
 
   useEffect(() => {
-    console.log("hello useEffect")
+    console.log('hello useEffect')
   }, [count])
+
+  const handleRef = () => {
+    if (!ref.current) return
+    console.log(ref.current.value)
+  }
 
   return (
     <>
       <List>
-        <ListItem sx={{display: "flex", flexDirection: "column", gap: 2}}>
+        <ListItem sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
           <Typography variant="h3">useState, useEffect</Typography>
-          <Button variant="outlined" onClick={handleClick}>+</Button>
+          <Button variant="outlined" onClick={handleClick}>
+            +
+          </Button>
           <Typography variant="body1">{count}</Typography>
         </ListItem>
         <Divider />
 
-        <ListItem sx={{display: "flex", flexDirection: "column", gap: 2}}>
+        <ListItem sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
           <Typography variant="h3">useContext</Typography>
           <Typography variant="body1">{userInfo.name}</Typography>
           <Typography variant="body1">{userInfo.age}</Typography>
         </ListItem>
         <Divider />
 
-      </List>      
+        <ListItem sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+          <Typography variant="h3">useRef</Typography>
+          <TextField type="text" inputRef={ref} />
+          <Button variant="outlined" onClick={handleRef}>
+            UseRef
+          </Button>
+        </ListItem>
+        <Divider />
+      </List>
     </>
   )
 }
